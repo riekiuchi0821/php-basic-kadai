d8e2ab7ff5e31fb75b4a382d83985b7c116260ed
riekiuchi0821 <rie.kiuchi.studio@gmail.com>
2026-07-13 15:26:04 +0900

    課題007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vegitable_1 = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($vegitable_1);
+        ?>
+    </p>
+</body>
+
+</html>
